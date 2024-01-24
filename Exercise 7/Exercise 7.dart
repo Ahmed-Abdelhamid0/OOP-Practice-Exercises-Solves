3c6b98f5340311a22ba5fefaa2500559fa97ef9c
Ahmed Abdelhamid <121736377+Ahmed-Abdelhamid0@users.noreply.github.com>
2024-01-24 15:12:49 +0200

    Add files via upload

---
+++ Exercise 7/Exercise 7.dart
@@ -0,0 +1,13 @@
+void main() {
+  /*********
+      Exercise :-
+
+      Create a simple quiz application using oop that allows users to play and view their score
+
+      Algorithm:-
+
+   *********/
+
+
+}
+
